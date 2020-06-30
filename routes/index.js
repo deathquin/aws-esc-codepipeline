21fba6fce64d7200a6d456b10f145a56ca2c4c11
deathquin <deathquin@gmail.com>
2020-06-30 17:03:59 +0900

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get("/health", async function(req, res){
+  res.writeHead(200, {
+    'Content-Type': 'text/plain' // "Content-Type": "text/html"
+  });
+
+  res.write('OK1211 Express App');
+  res.end();
+});
+
+module.exports = router;
