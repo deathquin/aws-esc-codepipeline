0bcedc80c515623657e712f1be76fd29d161b1f4
deathquin <deathquin@gmail.com>
2020-07-03 19:31:28 +0900

    First CopmT

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("HELLO1");
+  console.log("HELLO2");
   res.render('index', { title: 'Express' });
 });
 
