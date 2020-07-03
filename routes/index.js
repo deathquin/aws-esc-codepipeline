ecc7b905862ce8eebc576674a3d6bc9622838663
deathquin <deathquin@gmail.com>
2020-07-03 17:46:43 +0900

    테스트용

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("HELLO");
+  console.log("HELLO1");
   res.render('index', { title: 'Express' });
 });
 
